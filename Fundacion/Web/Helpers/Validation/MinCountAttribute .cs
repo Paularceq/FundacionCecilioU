ab9ecb94afebd02d5eb33b125876d4b84bac099b
Paula Arce <114875866+Paularceq@users.noreply.github.com>
2025-07-15 18:38:38 -0600

    Trabajo en la vista y en usuarios

---
+++ Fundacion/Web/Helpers/Validation/MinCountAttribute .cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.ComponentModel.DataAnnotations;
+
+namespace Web.Helpers.Validation
+{
+    public class MinCountAttribute : ValidationAttribute
+    {
+        private readonly int _min;
+
+        public MinCountAttribute(int min = 1)
+        {
+            _min = min;
+        }
+
+        public override bool IsValid(object value)
+        {
+            if (value is IList list)
+            {
+                return list.Count >= _min;
+            }
+
+            return false;
+        }
+
+        public override string FormatErrorMessage(string name)
+        {
+            return string.IsNullOrEmpty(ErrorMessage)
+                ? $"El campo {name} debe tener al menos {_min} elemento(s)."
+                : string.Format(ErrorMessage, name, _min);
+        }
+    }
+}
