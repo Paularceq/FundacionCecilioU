b38e0d2aa86dfccf69a7e0f809de2f64f006d3ff
Fabian <fabianleon.dev@gmail.com>
2025-11-19 10:03:50 -0600

    DummyEmailService

---
+++ Api/Services/Infrastructure/DummyEmailService.cs
@@ -0,0 +1,11 @@
+﻿using Api.Abstractions.Infrastructure;
+
+namespace Api.Services.Infrastructure;
+
+public class DummyEmailService : IEmailService
+{
+    public Task SendEmailAsync(string to, string subject, string body)
+    {
+        return Task.CompletedTask;
+    }
+}
